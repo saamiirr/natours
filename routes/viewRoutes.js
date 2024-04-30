aab0d2bc4730527deb6cf684b2c714f40dcef9c5
saamiirr <mohamedsamir3333333@gmail.com>
2024-04-30 13:49:36 +0300

    Improved stripe implementation

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,14 +6,14 @@ const router = express.Router();
 
 router.get(
   '/',
-  bookingController.createBookingCheckout,
+
   authController.isLoggedIn,
   viewsController.getOverview,
 );
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
-router.get('/my-tours', authController.protect, viewsController.getMyTours);
+// router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/sumbit-user-data',
